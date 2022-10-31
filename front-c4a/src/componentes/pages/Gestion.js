f06a7bab6ac8253e62da1d0c3376ab9e0800b40c
Sergio <sergiohervas18@gmail.com>
2022-10-31 21:27:42 +0100

    Breadcrumbs added

--- front-c4a/src/componentes/pages/Gestion.js
+++ front-c4a/src/componentes/pages/Gestion.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Header from '../Layout/Header.js';
 import Footer from '../Layout/Footer.js';
 import Body from '../Layout/Body.js';
+import { MuiBreadcrumbs } from '../MuiBreadcrumbs';
 import PantallasGestion from '../Layout/PantallasGestion';
 const Gestion = () => {
   return (
     <>
+    <MuiBreadcrumbs></MuiBreadcrumbs>
     <Header titulo="Gestión del centro"/>
     <PantallasGestion/>
     <Footer/>
