adf470666f2cb40549d473305c61b2cf0fb231ae
Javier Serrano <javier.serluc@gmail.com>
2022-11-04 20:38:37 +0100

    feat(core): Funcion helper para lanzar servidor

---
+++ API-REST/crearServidor.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {conexion} = require('./basedatos/conexion');
+const cors = require('cors');
+
+
+function crearSevidor(){
+    const app = express();
+    const puerto = 3900;
+
+    //Configurar cors middleware
+    app.use(cors());
+
+    //Lectura y parseo del body | parseamos toda peticion a un objeto json
+    app.use(express.json());                            //recibir datos con content-type app/json
+    app.use(express.urlencoded({extended:true}));       //recibir datos con content-type app/x-www-form-urlencoded
+
+    //Crear rutas pruebas
+
+    const rutas_usuario = require('./rutas/usuario');
+    const rutas_tarea = require('./rutas/tareasDia');
+    app.use("/api/usuarios", rutas_usuario);
+    app.use("/api/tareas", rutas_tarea);
+
+    return app;
+}
+//Crear servidor node
+
+
+module.exports = crearSevidor;
--- API-REST/index.js
+++ API-REST/index.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const {conexion} = require('./basedatos/conexion');
 const cors = require('cors');
+const crearSevidor = require('./crearServidor');
 
 
 console.log("App arrancada");
 
 //conectar base de datos
 conexion();
-
-//Crear servidor node
-const app = express();
 const puerto = 3900;
 
-//Configurar cors middleware
-app.use(cors());
-
-//Lectura y parseo del body | parseamos toda peticion a un objeto json
-app.use(express.json());                            //recibir datos con content-type app/json
-app.use(express.urlencoded({extended:true}));       //recibir datos con content-type app/x-www-form-urlencoded
-
-//Crear rutas pruebas
-
-const rutas_usuario = require('./rutas/usuario');
-const rutas_tarea = require('./rutas/tareasDia');
-app.use("/api/usuarios", rutas_usuario);
-app.use("/api/tareas", rutas_tarea);
-
-
+//Crear servidor node
+const app = crearSevidor();
 
 
 //Crear servidor y escuchar peticiones http
