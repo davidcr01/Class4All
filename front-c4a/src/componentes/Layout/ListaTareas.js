8178b43d7f791d18abab648792057568021d3cf0
Sergio <sergiohervas18@gmail.com>
2022-11-07 02:09:26 +0100

    Filtros empezados

--- front-c4a/src/componentes/Layout/ListaTareas.js
+++ front-c4a/src/componentes/Layout/ListaTareas.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Tareas from './Tareas.js';
 import '../../styles.css';
-class ListaTareas extends React.Component {
-    render(props) {
+const ListaTareas = (props) => {
+
+        function seleccionSinAg(){
+            //getelementbyid
+            document.getElementById('sa').style.backgroundColor = 'red';
+            props.cambio("sinasignar")
+        }
+
         return (
             <div className='ListaTareas'>
                 <div className="botones">
-                    <div className="boton">SIN ASIGNAR</div>
+                    <div className="boton" id="sa" onClick={seleccionSinAg}>SIN ASIGNAR</div>
                     <div className="boton">ASIGNADAS</div>
                     <div className="boton">REALIZADAS</div>
                     <div className="boton">NO REALIZADAS</div>
                 </div>
-                <Tareas tipo={this.props.tipo}/>
+                <Tareas tipo={props.tipo}/>
             </div>
+
         )
-    }
+
 
 }
 
