e0a3cf23bfe288e63c5f0c5b76923d89467dad18
David Correa <davidcorreafjb2001@gmail.com>
2022-11-07 19:04:55 +0100

    feat: vista tareas y fix: footer
    He mejorado un poquito el footer tb

--- front-c4a/src/componentes/Layout/Footer.js
+++ front-c4a/src/componentes/Layout/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
+    const style = {
+        textAlign: "center",
+        backgroundColor: "darkgray",
+        height: "auto",
+        padding: "15px 15px"
+    }
     return (
-        <div className="footer">
+        <div className="footer" style={style}>
             Class4All © 2022-2022 All rights reserved.
         </div>
     );
--- front-c4a/src/routing/Rutas.js
+++ front-c4a/src/routing/Rutas.js
@@ -8,6 +8,7 @@ import { SesionAlumnosClases} from '../componentes/pages/SesionAlumnosClases';
 import { SesionAlumnos} from '../componentes/pages/SesionAlumnos';
 import { GestionUsuarios} from '../componentes/pages/GestionUsuarios';
 import { SesionProfesores } from '../componentes/pages/SesionProfesores';
+import { Tareas } from '../componentes/pages/Tareas';
 export const Rutas = () => {
     return (
         <BrowserRouter>
@@ -22,6 +23,7 @@ export const Rutas = () => {
             <Route path="/sesion-alumnos-clases" element={<SesionAlumnosClases/>} />
             <Route path="/sesion-alumnos" element={<SesionAlumnos/>} />
             <Route path="/sesion-profesores" element={<SesionProfesores/>} />
+            <Route path="/tareas" element={<Tareas/>} />
             {/* <Route path="/otra-pagina" element={<OtraPagina />} /> */}
         </Routes>
 
