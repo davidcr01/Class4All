511cfecf3ea6f89b60524021587ae8f1fbe532c7
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-11-09 00:27:56 +0100

    Inicio de sesion para alumnos funcional

--- front-c4a/src/componentes/pages/SesionAlumnosClases.js
+++ front-c4a/src/componentes/pages/SesionAlumnosClases.js
@@ -1,12 +1,41 @@
 import '../../styles.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../Layout/Header.js';
 import Footer from '../Layout/Footer.js';
 import Body from '../Layout/Body.js';
 import { MuiBreadcrumbs } from '../muibreadcrumbs';
 import Clases from '../Layout/Clases';
+import Cookies from "universal-cookie";
+import { isCookieSet } from '../../interfazCookies/cookies';
 
 export const SesionAlumnosClases = () => {
+
+  const cookies = new Cookies();
+  const [cargando, setCargando] = useState(true);
+  const [isSet, setIsSet] = useState(false);
+
+  useEffect(() => {
+    isCookieSet().then((res) => {
+      setIsSet(res);
+      setCargando(false);
+    });
+  }, []);
+
+  if(cargando)
+    return (
+      <div>
+        <h1>CARGANDO...</h1>
+      </div>
+    )
+
+    else if(cookies.get("loginCookie") !== undefined && isSet){
+      return (
+        <div>
+          <h1>SESION YA INICIADA</h1>
+        </div>
+      )      
+    }
+  else
   return (
     <>
     <Header titulo="Inicio De Sesión"/>
@@ -14,6 +43,7 @@ export const SesionAlumnosClases = () => {
     <Clases/>
     <Footer/>
   </>)
+
 };
 
 //export default SesionAlumnosClases;
