0775714db67dbb2f2ab63687889194dfa991b8f1
Javier Serrano <javier.serluc@gmail.com>
2022-11-10 11:37:16 +0100

    perf(API): Eliminadas rutas de prueba

--- API-REST/rutas/usuario.js
+++ API-REST/rutas/usuario.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const UsuarioController = require('../controladores/usuario.js');
-//Rutas prueba
-router.get("/ruta-de-prueba" , UsuarioController.prueba);
-router.get("/datos-empresa" , UsuarioController.datosEmpresa);
+
+
 router.post("/crear-usuario" , UsuarioController.crear);
 router.get("/listar-usuarios", UsuarioController.listar);
 router.get("/user/:id", UsuarioController.obtenerUsuarioId);
