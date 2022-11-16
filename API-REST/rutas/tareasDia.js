ca8892f72234cda8b808fc3f8dc6d17d8b165b61
Javier Serrano <javier.serluc@gmail.com>
2022-11-16 14:57:10 +0100

    Refactor(API): Estandarizacion rutas tareas

--- API-REST/rutas/tareasDia.js
+++ API-REST/rutas/tareasDia.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const TareaController = require('../controladores/tareaDia.js');
 
 router.get("/lista-tareasDia" , TareaController.listaTareas);
-router.get("/tarea/:idTarea" , TareaController.obtenerTarea);
-router.get("/usuario/:idUsuario" , TareaController.obtenerTareasUsuario);
-router.get("/foto/:idTarea" , TareaController.obtenerFoto);
+router.get("/get-tarea/:idTarea" , TareaController.obtenerTarea);
+router.get("/tareas-usuario/:idUsuario" , TareaController.obtenerTareasUsuario);
+router.get("/get-foto/:idTarea" , TareaController.obtenerFoto);
 
 router.post("/crear-tareaDia" , TareaController.crearTarea);
 router.delete("/eliminar-tareaDia/:id" , TareaController.eliminarTarea);
