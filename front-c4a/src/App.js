6529c83ab067cd5d115e5cdfa6e6ad014264d343
Javier Serrano <javier.serluc@gmail.com>
2022-11-19 13:57:11 +0100

    feat(Core): Añadido sistema de eleccion routers

--- front-c4a/src/App.js
+++ front-c4a/src/App.js
@@ -1,13 +1,35 @@
-// import './App.css';
-//import Header from './componentes/Layout/Header.js';
-//import Footer from './componentes/Layout/Footer.js';
-//import Body from './componentes/Layout/Body.js';
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+
+
 import {Rutas} from './routing/Rutas.js';
 import {ContextoRol} from './Contexto/Roles';
-
+import {RouterAdmins} from './routing/RouterAdmins.js';
+import {RouterInicioSesion} from './routing/RouterInicioSesion.js';
+import {RouterAlumnos} from './routing/RouterAlumnos.js';
 
 function App() {
+  const [cookie, setCookie] = useState(null);
+  const [rol, setRol] = useState(null);
+
+  const ChangeRol = () => {
+    if(cookie === 'admin')
+      setRol('admin');
+    else{
+      if(cookie === 'alumno')
+        setRol('alumno');
+      else{
+        if(cookie === 'profesor')
+          setRol('profesor');
+        else
+          setRol(null);
+        
+      }
+    }
+    
+  };
+
+
+
   return (
     <Rutas />
   );
