01e5799392bf5a269b0c6b1b93a11fc0ccce77d1
Javier Serrano <javier.serluc@gmail.com>
2022-11-24 18:48:36 +0100

    Perf(Mats-Alumno): estructura

--- front-c4a/src/componentes/alumnos/pages/EntregaMaterial.js
+++ front-c4a/src/componentes/alumnos/pages/EntregaMaterial.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useParams } from "react-router-dom"
+
+import Header from '../../compartido/Layout/Header';
+import CargandoProgress from '../../compartido/Layout/CargandoProgress';
+import { FlechasPaginacionGenerico } from '../../flechasPaginacionGenerico';
 
 export const EntregaMaterial = () => {
-  return (
-    <div>EntregaMaterial</div>
-  )
+
+    const { id } = useParams();
+    const [materiales, setMateriales] = useState([]);
+    const [currentMaterial, setcurrentMaterial] = useState(0)//indice de la estructura de tareas
+    const [cargando, setCargando] = useState(true);
+
+    const materialesIncrement = 1;
+
+    const rellenarMateriales = async () => {
+        let url = 'http://localhost:3900/api/tareas/get-tarea/' + id;
+        try {
+            let res = await fetch(url);
+            let data = await res.json();
+            setMateriales(data.tarea.entregamateriales.materiales);
+            setCargando(false);
+        }
+        catch (error) {
+            console.log(error);
+            console.log("Error al rellenar materiales");
+        }
+
+    };
+
+    useEffect(() => {
+        rellenarMateriales();
+    }, []);
+
+    if(cargando){
+        return(
+            <CargandoProgress/>
+        )
+    }
+    else{
+        return (
+            <>
+             <Header titulo="Materiales" /><div className='PaginaAgenda'></div>
+             <section>
+                <h2>Pictogramas</h2>
+             </section>
+
+             <FlechasPaginacionGenerico currentIndex={currentMaterial} setCurrentIndex={setcurrentMaterial} length={materiales.length} increment={materialesIncrement} />
+             <section>
+                <h2>materiales</h2>
+             </section>
+
+             <section>
+                <h2>aceptar/ rechazar</h2>
+             </section>
+            
+            </>
+          )
+    }
+  
 }
