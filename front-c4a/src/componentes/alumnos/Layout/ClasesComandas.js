675cb154d361ccb8cd3f574b22ab534163add593
dacal01 <daelcago2001@gmail.com>
2022-11-25 10:24:14 +0100

    Añadida vista menús alumnos y paginación

---
+++ front-c4a/src/componentes/alumnos/Layout/ClasesComandas.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
+// Componente para seleccionar la clase para iniciar sesión como alumno
+// Vista: alumno
+
+const Clases = ({aulas}) => {
+
+
+        if(aulas === undefined || aulas.length === 0){
+            return (
+                <h1>NO HAY AULAS EN EL SISTEMA</h1>
+            )
+        }
+        else{
+            const nav = useNavigate();
+
+            const goToComandas = (route) => {
+                nav("/comandas", {state: {aula: route}});
+            }
+            let res = [];
+
+            for(let i=0; i<aulas.length; i++){
+                res.push(
+                    <button key={aulas[i].clase} className="botonesClase" onClick={() => goToComandas(aulas[i].clase)}>
+                        <img className="foto" src={"http://localhost:3900/api/usuarios/get-foto/"+aulas[i].id}/>
+                        {"Aula "+aulas[i].clase}
+                    </button>
+                )
+            }
+
+        return (
+            <div className="cuerpo">
+                <div className="recuadrosclases">
+                    {res}
+                </div>
+                
+            </div>
+        )
+        }
+
+}
+
+export default Clases;
