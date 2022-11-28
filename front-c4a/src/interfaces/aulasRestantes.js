eab994ad61684ae8683c74c4078ae571656570a3
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-11-28 20:47:02 +0100

    Creada la documentacion para la interfaz

--- front-c4a/src/interfaces/aulasRestantes.js
+++ front-c4a/src/interfaces/aulasRestantes.js
@@ -1,5 +1,10 @@
 
-
+/**
+ * Obtiene las aulas restantes para una tarea dada.
+ * @pre La tarea debe ser del tipo comanda.
+ * @param idTarea Identificador de la tarea.
+ * @returns Array vacío en caso de no poder obtener aulas restantes. En otro caso el array con las aulas que quedan.
+ */
 export const getAulasRestantes = async (idTarea) => {
     try {
         const url = "http://localhost:3900/api/tareas/aulas-restantes-comanda/"+idTarea;
@@ -15,6 +20,16 @@ export const getAulasRestantes = async (idTarea) => {
     }
 }
 
+/**
+ * Elimina una aula de las aulas pendientes que tiene un alumno para las comandas.
+ * @pre La tarea debe ser del tipo comanda.
+ * @param idTarea Identificador de la tarea
+ * @param aula Aula que se desea marcar como completada
+ * @returns true en caso de haberse llevado con exito el cambio. false en caso de no haberse llevado con exito.
+ * 
+ * @note Cabe destacar que esta operacion permite la eliminacion del aula multiple, es decir, si se tienen estas aulas
+ * [3A, 2B] y se mandan dos veces eliminar 3A, el resultado sigue siendo [2B].
+ */
 export const setAulaCompletada = async (idTarea, aula) => {
     try {
         const url = "http://localhost:3900/api/tareas/completar-clase-comanda";
