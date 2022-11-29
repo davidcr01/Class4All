d87665a06a3362aa2f9952ca5ef0534a9dd7947b
Javier Serrano <javier.serluc@gmail.com>
2022-11-29 17:36:31 +0100

    feat(Front): added cantidad y matts

--- front-c4a/src/componentes/alumnos/pages/EntregaMaterial.js
+++ front-c4a/src/componentes/alumnos/pages/EntregaMaterial.js
@@ -5,12 +5,17 @@ import Header from '../../compartido/Layout/Header';
 import CargandoProgress from '../../compartido/Layout/CargandoProgress';
 import { FlechasPaginacionGenerico } from '../../flechasPaginacionGenerico';
 
+import {getImage} from '../../../interfaces/arasaac'
+import imagenesARASAAC from "../../../img/imagenesARASAAC.json";
+import Button from '@mui/material/Button';
+
 export const EntregaMaterial = () => {
 
     const { id } = useParams();
     const [materiales, setMateriales] = useState([]);
     const [currentMaterial, setcurrentMaterial] = useState(0)//indice de la estructura de tareas
     const [cargando, setCargando] = useState(true);
+    const [Profe , setProfe] = useState({});
 
     const materialesIncrement = 1;
 
@@ -20,6 +25,7 @@ export const EntregaMaterial = () => {
             let res = await fetch(url);
             let data = await res.json();
             setMateriales(data.tarea.entregamateriales.materiales);
+            await rellenaProfe(data.tarea.entregamateriales.idProfesor);
             setCargando(false);
         }
         catch (error) {
@@ -27,6 +33,19 @@ export const EntregaMaterial = () => {
             console.log("Error al rellenar materiales");
         }
 
+    };
+
+    const rellenaProfe = async (id) => {
+        let url = 'http://localhost:3900/api/usuarios/get-usuario/' + id;
+        try {
+            let res = await fetch(url);
+            let data = await res.json();
+            setProfe(data.usuario);
+        }
+        catch (error) {
+            console.log(error);
+            console.log("Error al rellenar profe");
+        }
     };
 
     useEffect(() => {
@@ -43,18 +62,30 @@ export const EntregaMaterial = () => {
             <>
              <Header titulo="Materiales" alumnos="si"/><div className='PaginaAgenda'></div>
              <section>
-                <h2>Pictogramas</h2>
+                <figure>
+                    <img src={"https://api.arasaac.org/api/pictograms/31758"} alt={"voy a"} />
+                </figure>
+                <figure>
+                    <img src={"http://localhost:3900/api/usuarios/get-foto/"+ Profe._id} alt={"Clase de " + Profe.nombre} />
+                </figure>
              </section>
 
 
              <FlechasPaginacionGenerico currentIndex={currentMaterial} setCurrentIndex={setcurrentMaterial} length={materiales.length} increment={materialesIncrement} />
 
              <section>
-                <h2>materiales</h2>
+                <figure>
+                    <img src={getImage(imagenesARASAAC['numeros'][materiales[currentMaterial].cantidad])} alt={"Material " + materiales[currentMaterial].idMaterial} />
+                </figure>
+                <figure>
+                    <img src={"http://localhost:3900/api/materials/obtenerfoto/"+ materiales[currentMaterial].material} alt={"XD"} />
+                </figure>
              </section>
 
              <section>
-                <h2>aceptar/ rechazar</h2>
+                <Button variant="contained" onClick={e => paginaAdelante()}>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flecha_tesela.svg/1200px-Flecha_tesela.svg.png' />
+                </Button>
              </section>
             
             </>
