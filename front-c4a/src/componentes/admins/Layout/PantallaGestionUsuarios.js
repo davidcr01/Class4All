ff8203adcf442feed2560cf4b5b8f45a0abc5b86
Sergio <sergiohervas18@gmail.com>
2022-12-02 10:16:39 +0100

    Filtros casi

--- front-c4a/src/componentes/admins/Layout/PantallaGestionUsuarios.js
+++ front-c4a/src/componentes/admins/Layout/PantallaGestionUsuarios.js
@@ -2,17 +2,28 @@ import React from 'react';
 import '../styles.css';
 import BuscarFiltros from '../../compartido/Layout/Tareas/BuscarFiltros';
 import ListaUsuarios from '../../admins/Layout/ListaUsuarios';
+import {useState} from 'react'
 
 // Vista: admins
 
-const PantallaGestionUsuarios = ()=> {
-    return(
-    <div className='PantallaGestionUsuarios'>
-        <BuscarFiltros/>
+const PantallaGestionUsuarios = () => {
+    const [filtro, setFiltro] = useState(0);
 
-        <ListaUsuarios/>
-    </div>
-    )
+    const parentToChild = () => {
+
     }
+
+    const childToParent = (i) => {
+        setFiltro(i);
+    }
+    return (
+
+        <div className='PantallaGestionUsuarios'>
+            <BuscarFiltros childToParent={childToParent}/>
+
+            <ListaUsuarios />
+        </div>
+    )
+}
 
 export default PantallaGestionUsuarios;
--- front-c4a/src/componentes/compartido/Layout/Tareas/BuscarFiltros.js
+++ front-c4a/src/componentes/compartido/Layout/Tareas/BuscarFiltros.js
@@ -3,7 +3,25 @@ import '../../styles.css';
 
 // Componente para buscar filtros en la gestión de las tareas
 // Vista: compartido (admins y profes)
-const BuscarFiltros = (props) => {
+const BuscarFiltros = ({childToParent(i)}) => {
+    const data = 0;
+
+    const aplicarFiltro = (n, e) => {
+        
+        
+        let valor = e.target.value;
+        console.log(valor);
+        if(valor === "on"){
+            childToParent(n);
+        }
+        else{
+            childToParent(0);
+        }
+        
+
+    }
+
+
 
     return (
         <div className="divfiltros">
@@ -11,10 +29,12 @@ const BuscarFiltros = (props) => {
                 <label className="buscarlabel">BUSCAR
                     <input className="filtrotexto" type="text" /></label>
                 <div className='Filtros'>Filtros:</div>
-                <div class="checkboxs"><label htmlFor="a" className="checkbox"><input type="checkbox" id="a" />OPCION A</label>
-                    <label htmlFor="b" className="checkbox"><input type="checkbox" id="b" />OPCION B</label>
-                    <label htmlFor="c" className="checkbox"><input type="checkbox" id="c" />OPCION C</label>
-                </div></div>
+                <div class="checkboxs">
+                    <label htmlFor="a" className="checkbox"><input type="checkbox" id="1" onClick={e => aplicarFiltro(1,e)}/>Alumnos</label>
+                    <label htmlFor="b" className="checkbox"><input type="checkbox" id="2" onClick={e => aplicarFiltro(2,e)}/>Administradores</label>
+                    <label htmlFor="c" className="checkbox"><input type="checkbox" id="3" onClick={e => aplicarFiltro(3,e)}/>Profesores</label>
+                </div>
+            </div>
         </div>
     )
 
