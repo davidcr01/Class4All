0d7960b43a33de413e28d45eb55f88f0091abe9e
Sergio <sergiohervas18@gmail.com>
2022-12-12 00:44:25 +0100

    Letras pequeñas y agenda

--- front-c4a/src/interfaces/tamaños.js
+++ front-c4a/src/interfaces/tamaños.js
@@ -3,18 +3,21 @@ import React from 'react';
 
 function setTams(newTam) {
     console.log("nt:" + newTam);
-    let tammediano = 2.5;
+    let tammediano = 2.5, tampequeño = 1.75;
     if (newTam === 1) {
         tammediano = 1.5;
-        
+        tampequeño = 1;
     }
     else if (newTam === 2) {
         tammediano = 2.5;
+        tampequeño = 1.75;
     }
     else {
         tammediano = 3.5;
+        tampequeño = 2.5;
     }
     document.documentElement.style.setProperty('--tam-letra-mediana', tammediano + 'vw');
+    document.documentElement.style.setProperty('--tam-letra-pequena', tampequeño + 'vw');
 }
 
 
